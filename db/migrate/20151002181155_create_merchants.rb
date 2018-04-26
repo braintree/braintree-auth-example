9538fcf563298043cf93b4e7286957a6b78cebab
Timothy Martinak <timothy.martinak@getbraintree.com>
2018-04-26 21:43:05 +0000

    Fix Migrations
    `sinatra-activerecord` has the dependency `activerecord (>=3.2)` which
    caused active record 5.1+ to be used.  5.1 no longer allows inhereting
    directly from `ActiveRecord::Migration`, so pinning the migrations to
    5.0 fixes this.

--- db/migrate/20151002181155_create_merchants.rb
+++ db/migrate/20151002181155_create_merchants.rb
@@ -1,4 +1,4 @@
-class CreateMerchants < ActiveRecord::Migration
+class CreateMerchants < ActiveRecord::Migration[5.0]
   def change
     create_table :merchants do |t|
       t.string :email
--- db/migrate/20160809160330_add_country_code.rb
+++ db/migrate/20160809160330_add_country_code.rb
@@ -1,4 +1,4 @@
-class AddCountryCode < ActiveRecord::Migration
+class AddCountryCode < ActiveRecord::Migration[5.0]
   def change
     add_column :merchants, :country_code, :string, :default => 'USA'
   end
